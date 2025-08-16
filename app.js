d7a0612f394041ec8cd8184a70dbfcd8348b7206
bishaldsrija08 <bishaldsrijal1@gmail.com>
2025-08-16 10:29:38 +0545

    Implement user and blog creation endpoints with validation; add user and blog models

--- app.js
+++ app.js
@@ -1,22 +1,55 @@
 //Env config
-const envConfig = require("dotenv").config()
+require("dotenv").config()
 
 // Require express
 const express = require("express")
 
 //Db connect import
 const connectToDatabase = require("./database")
+const User = require("./model/useModel")
+const Blog = require("./model/blogModel")
 const app = express()
 
 connectToDatabase() // Connect to the database
 
+// parse json
+app.use(express.json())
 
-// Simple get api
-app.get("/", (req,res)=>{
-    console.log("Hello")
-    res.status(200).json({
-        message: "Welcome to the API."
+// Create User
 
+app.post("/create", async (req, res) => {
+    const { username, password, email } = req.body
+    if (!username || !password || !email) {
+        return res.status(400).json({
+            message: "All fields are required."
+        })
+    }
+    await User.create({
+        username,
+        password,
+        email
+    })
+    return res.status(200).json({
+        message: "User created successfully."
+    })
+})
+
+//create blog
+
+app.post("/blog", async (req, res) => {
+    const { description, title, subTitle } = req.body
+    if (!description || !title || !subTitle) {
+        return res.status(400).json({
+            message: "All fields are required."
+        })
+    }
+    await Blog.create({
+        description,
+        title,
+        subTitle
+    })
+    return res.status(200).json({
+        message: "Blog created successfully."
     })
 })
 
@@ -30,6 +63,6 @@ app.get("/", (req,res)=>{
 
 
 const port = process.env.PORT
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`Server is running on port ${port}.`)
 })
